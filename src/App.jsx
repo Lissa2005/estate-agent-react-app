f2e054c73157efae258fe989b6d23027dd21eb58
Lissa2005 <mellissamangalaari2@gmail.com>
2026-01-04 21:32:02 +0530

    Update App.jsx
    added a bit of code and made it neater

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,28 @@ import PropertyCard from "./components/PropertyCard";
 
 function App(){
   return(
-    <div className="container">
-
-      <div className="all-items">
-        <h1>Property Listings</h1>
+    <div className="app">
+      <div className="container">
         
-        <div className="gallery">
-          {data.properties.map(property => (
-            <PropertyCard key={property.id}property={property}/>
-            ))}
-        </div>
-      </div>
-      
-      <div className="favorites">
-        <h2>Favourites</h2>
-        <div className="gallery">
-          {/*Favourites will go here later*/}
-        </div>
-      </div>
+        <div className="all-items">
+          <h1>Property Listings</h1>
 
+          <div className="gallery">
+            {data.properties.map(property => (
+              <PropertyCard 
+              key={property.id}
+              property={property}/>
+              ))}
+          </div>
+        </div>
+        
+        <div className="favorites">
+          <h2>Favourites</h2>
+          <div className="gallery">
+            {/*Favourites will go here later*/}
+          </div>
+        </div>
+      </div>
     </div>      
   );
 }
