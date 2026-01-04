a1b3bff0e0f99d9da61de8da1b4e53d7a176e83c
Lissa2005 <mellissamangalaari2@gmail.com>
2026-01-04 21:05:51 +0530

    Updated property Card

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,21 +1,23 @@
 function PropertyCard({ property }){
     return(
         <div className="card">
-
             <div className="image">
-                <img
-                    src={property.images?.[0]}
-                    alt={property.type}
+                <img 
+                    src={property.picture}
+                    alt={'${property.type} in ${property.location}'}
                 />
             </div>
-            
+
             <div className="description">
                 <h3>{property.type}</h3>
-                <p>£{property.price}</p>
-                <p>{property.bedrooms}</p>
+                <p><strong>Price:</strong> £{property.price.toLocaleString()}</p>
+                <p><strong>Bedrooms:</strong> {property.bedrooms}</p>
+                <p><strong>Tenture:</strong> {property.tenture}</p>
                 <p>{property.location}</p>
+                <small>
+                    Added: {property.added.day} {property.added.month} {property.added.year}
+                </small>
             </div>
-            
         </div>
     );
 }
