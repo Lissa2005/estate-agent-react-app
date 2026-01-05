9959350889831069b026180e8d346ae9cf0a860a
Lissa2005 <mellissamangalaari2@gmail.com>
2026-01-05 07:56:45 +0530

    Update PropertyDetails.jsx
    Code for the property details

---
+++ src/pages/PropertyDetails.jsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import data from "../data/properties.json";
+import ImageGallery from "../components/ImageGallery";
+import Tabs from "../components/Tabs";
+
+function PropertyDetails(){
+    
+    const{id} =useParams();
+    const property =data.properties.find(p => p.id === id);
+
+    if (!property)return <p>Property not found.</p>;
+
+    return (
+        <div className="property-details">
+
+            <h1>{Property.type}</h1>
+            <p>{property.location}</p>
+            <p>£{property.price.toLocalString()}</p>
+            
+            <ImageGallery images={property.images || [property.picture]} />
+
+            <Tabs property={property} />
+
+        </div>
+    )
+
+};
+
+export default PropertyDetails;
