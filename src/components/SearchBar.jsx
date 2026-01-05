cc06f703b23795cfd1250111c09feee1d1a8a494
Lissa2005 <mellissamangalaari2@gmail.com>
2026-01-05 10:35:25 +0530

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,58 +1,64 @@
-function SearchBar({ filters,setFilters}) {
+function SearchBar({ filters, setFilters }) {
 
-    function handleChange(event){
-        setFilters({
-            ...filter,
-            [event.target.name]: event.target.value
-        });
-    }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFilters({
+      ...filters,
+      [name]: value
+    });
+  }
 
-    return(
-        <div className="filter">
+  return (
+    <section className="search-container">
+      <h1>Property Search</h1>
 
-            <h1>Property Search</h1>
+      {/* GRID WRAPPER */}
+      <div className="filter">
 
-            <label>Type</label>
-
-            <select name="type" onChange={handleChange}>
-                <option value="">Any</option>
-                <option value="House">House</option>
-                <option value="Flat">Flat</option>
-            </select>
-
-            <div>
-            <label>Min Price</label>
-            <input type="number" name="minPrice" onChange={handleChange}/>
-            </div>
-
-            <div>
-            <label>Max Price</label>
-            <input type="number" name="maxPrice" onChange={handleChange}/>
-            </div>
-
-            <div>
-            <label>Min Bedrooms</label>
-            <input type="number" name="minBeds" onChange={handleChange}/>
-            </div>
-
-            <div>
-            <label>Max Bedrooms</label>
-            <input type="number" name="maxBeds" onChange={handleChange}/>
-            </div>
-
-            <div>
-            <label>Postcode Area</label>
-            <input type="text" name="posrcode" onChange={handleChange} />
-            </div>
-
-            <div>
-            <label>Added After</label>
-            <input type="date" name="afterDate" onChange={handleChange} />
-            </div>
+        <div>
+          <label>Type</label>
+          <select name="type" onChange={handleChange} value={filters.type || ""}>
+            <option value="">Any</option>
+            <option value="House">House</option>
+            <option value="Flat">Flat</option>
+            <option value="Bungalow">Bungalow</option>
+            <option value="Maisonette">Maisonette</option>
+          </select>
         </div>
 
+        <div>
+          <label>Min Price</label>
+          <input type="number" name="minPrice" onChange={handleChange} />
+        </div>
 
-    );
+        <div>
+          <label>Max Price</label>
+          <input type="number" name="maxPrice" onChange={handleChange} />
+        </div>
+
+        <div>
+          <label>Min Bedrooms</label>
+          <input type="number" name="minBedrooms" onChange={handleChange} />
+        </div>
+
+        <div>
+          <label>Max Bedrooms</label>
+          <input type="number" name="maxBedrooms" onChange={handleChange} />
+        </div>
+
+        <div>
+          <label>Postcode Area</label>
+          <input type="text" name="postcode" onChange={handleChange} />
+        </div>
+
+        <div>
+          <label>Added After</label>
+          <input type="date" name="afterDate" onChange={handleChange} />
+        </div>
+
+      </div>
+    </section>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
