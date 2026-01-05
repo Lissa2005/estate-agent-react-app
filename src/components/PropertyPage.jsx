7117d5be1592e586d0effb6fcb2d9b9ff34cfc08
Lissa2005 <mellissamangalaari2@gmail.com>
2026-01-05 11:01:50 +0530

    Update PropertyPage.jsx

--- src/components/PropertyPage.jsx
+++ src/components/PropertyPage.jsx
@@ -6,17 +6,23 @@ function PropertyPage() {
   const { id } = useParams();
   const property = data.properties.find(p => p.id === id);
 
-  if (!property) return <p>Property not found</p>;
+  if (!property) return <h2>Property not found</h2>;
 
   return (
     <div className="property-page">
-      <h1>{property.type}</h1>
-      <img src={property.picture} alt={property.type} />
-      <p><strong>Price:</strong> £{property.price.toLocaleString()}</p>
-      <p>{property.location}</p>
 
-      {/* TABS USED HERE */}
+      <h1>{property.type}</h1>
+      <p>{property.location}</p>
+      <h2>£{property.price.toLocaleString()}</h2>
+
+      <img
+        src={property.picture}
+        alt={property.type}
+        style={{ width: "100%", borderRadius: "20px" }}
+      />
+
       <Tabs property={property} />
+
     </div>
   );
 }
